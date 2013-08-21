b25d85bd32ed37a24ec2714d8de582b062c837b4
Daniele Sluijters <daniele.sluijters@nedap.com>
2013-08-21 16:57:12 -0700

    dev: Import DEV_LISTEN_HOST

--- dev.py
+++ dev.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 from __future__ import absolute_import
 
 from puppetboard.app import app
-from puppetboard.default_settings import DEV_LISTEN, DEV_LISTEN_PORT
+from puppetboard.default_settings import DEV_LISTEN_HOST, DEV_LISTEN_PORT
 
 if __name__ == '__main__':
     app.debug=True
