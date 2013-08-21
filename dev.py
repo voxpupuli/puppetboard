cb64b738322710d3ee449c0bc9de7845665186ad
Spencer Krum <skrum@go2uti.com>
2013-08-21 11:01:37 -0700

    dev.py: exposing some parameters

--- dev.py
+++ dev.py
@@ -2,7 +2,8 @@ from __future__ import unicode_literals
 from __future__ import absolute_import
 
 from puppetboard.app import app
+from puppetboard.default_settings import DEV_LISTEN, DEV_LISTEN_PORT
 
 if __name__ == '__main__':
     app.debug=True
-    app.run('127.0.0.1')
+    app.run(DEV_LISTEN_HOST, DEV_LISTEN_PORT)
