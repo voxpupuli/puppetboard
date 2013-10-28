voxpupuli/puppetboard

commit 795d243e9d998d917282371cbf5bac7c9f0e6d83

We now require PuppetDB 1.5 / API v3.
PUPPETDB_API is no longer configurable since we're now using features
that are v3 only. Limiting ourselves to v2 compatibility is far too
troublesome and people tend to update to newer versions of PuppetDB
fairly quickly.

puppetboard/app.py
@@ -27,7 +27,7 @@ app.config.from_envvar('PUPPETBOARD_SETTINGS', silent=True)
 app.secret_key = os.urandom(24)
 
 puppetdb = connect(
-        api_version=app.config['PUPPETDB_API'],
+        api_version=3,
         host=app.config['PUPPETDB_HOST'],
         port=app.config['PUPPETDB_PORT'],
         ssl=app.config['PUPPETDB_SSL'],

puppetboard/default_settings.py
@@ -4,7 +4,6 @@ PUPPETDB_SSL=False
 PUPPETDB_KEY=None
 PUPPETDB_CERT=None
 PUPPETDB_TIMEOUT=20
-PUPPETDB_API=3
 DEV_LISTEN_HOST='127.0.0.1'
 DEV_LISTEN_PORT=5000
 UNRESPONSIVE_HOURS=2

commit 75da9b92092b7c2e76b0655121eac3e2834484a1

Get rid of old settings.
I broke things with 795d243e9d998d917282371cbf5bac7c9f0e6d83 because I
forgot to remove it everywhere from functions and templates. Also
removed the old PUPPETDB_EXPERIMENTAL switching.

puppetboard/app.py
@@ -137,10 +137,7 @@ def node(node_name):
     """
     node = get_or_abort(puppetdb.node, node_name)
     facts =  node.facts()
-    if app.config['PUPPETDB_API'] > 2:
-        reports = ten_reports(node.reports())
-    else:
-        reports = iter([])
+    reports = ten_reports(node.reports())
     return render_template('node.html', node=node, facts=yield_or_stop(facts),
             reports=yield_or_stop(reports))
 
@@ -148,22 +145,14 @@ def node(node_name):
 def reports():
     """Doesn't do much yet but is meant to show something like the reports of
     the last half our, something like that."""
-    if app.config['PUPPETDB_API'] > 2:
-        return render_template('reports.html')
-    else:
-        log.warn('PuppetDB API prior to v3 cannot access reports.')
-        abort(412)
+    return render_template('reports.html')
 
 @app.route('/reports/<node>')
 def reports_node(node):
     """Fetches all reports for a node and processes them eventually rendering
     a table displaying those reports."""
-    if app.config['PUPPETDB_API'] > 2:
-        reports = ten_reports(yield_or_stop(
-            puppetdb.reports('["=", "certname", "{0}"]'.format(node))))
-    else:
-        log.warn('PuppetDB API prior to v3 cannot access reports.')
-        abort(412)
+    reports = ten_reports(yield_or_stop(
+        puppetdb.reports('["=", "certname", "{0}"]'.format(node))))
     return render_template('reports_node.html', reports=reports,
             nodename=node)
 
@@ -175,10 +164,7 @@ def report_latest(node_name):
     """
     # TODO: use limit parameter in _query to get just one report
     node = get_or_abort(puppetdb.node, node_name)
-    if app.config['PUPPETDB_API'] > 2:
-        reports = ten_reports(node.reports())
-    else:
-        reports = iter([])
+    reports = ten_reports(node.reports())
     report = list(yield_or_stop(reports))[0]
     return redirect(url_for('report', node=node_name, report_id=report))
 
@@ -187,11 +173,7 @@ def report(node, report_id):
     """Displays a single report including all the events associated with that
     report and their status.
     """
-    if app.config['PUPPETDB_API'] > 2:
-        reports = puppetdb.reports('["=", "certname", "{0}"]'.format(node))
-    else:
-        log.warn('PuppetDB API prior to v3 cannot access reports.')
-        abort(412)
+    reports = puppetdb.reports('["=", "certname", "{0}"]'.format(node))
 
     for report in reports:
         if report.hash_ == report_id:

puppetboard/templates/node.html
@@ -18,16 +18,13 @@
           <td>Facts retrieved at</td>
           <td rel="utctimestamp">{{node.facts_timestamp}}</td>
         </tr>
-        {% if config.PUPPETDB_EXPERIMENTAL %}
         <tr>
           <td>Report uploaded at</td>
           <td rel="utctimestamp">{{node.report_timestamp}}</td>
         </tr>
-        {% endif %}
       </tbody>
     </table>
   </div>
-  {% if config.PUPPETDB_API > 2 %}
   <div class="span4">
     <h1>Facts</h1>
     {{macros.facts_table(facts, condensed=True, margin_top=10)}}
@@ -36,11 +33,5 @@
     <h1>Reports</h1>
     {{ macros.reports_table(reports, node.name, condensed=True, hash_truncate=True, show_conf_col=False, show_agent_col=False, show_host_col=False)}}
   </div>
-  {% else %}
-  <div class="span8">
-    <h1>Facts</h1>
-    {{macros.facts_table(facts, condensed=True, margin_top=10)}}
-  </div>
-  {% endif %}
 </div>
 {% endblock content %}

puppetboard/templates/nodes.html
@@ -21,10 +21,8 @@
       <th>Status</th>
       <th>Hostname</th>
       <th>Catalog compiled at</th>
-      {% if config.PUPPETDB_API > 2 %}
       <th>Last report</th>
       <th>&nbsp;</th>
-      {% endif %}
     </tr>
   </thead>
   <tbody class="searchable">
@@ -38,7 +36,6 @@
       </td>
       <td><a href="{{url_for('node', node_name=node.name)}}">{{node.name}}</a></td>
       <td rel="utctimestamp">{{node.catalog_timestamp}}</td>
-      {% if config.PUPPETDB_API > 2 %}
       <td>
         {% if node.report_timestamp %}
         <span rel="utctimestamp">{{ node.report_timestamp }}</span>
@@ -52,7 +49,6 @@
         <a class="btn btn-small btn-primary" href="{{url_for('reports_node', node=node.name)}}">Reports</a>
         {% endif %}
       </td>
-      {% endif %}
     </tr>
     {% endfor %}
   </tbody>