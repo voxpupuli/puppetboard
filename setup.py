8b0a797097e1c20d92a8fc093b768c3362c823c9
Corey Hammerton <corey.hammerton@gmail.com>
2015-11-12 19:50:28 -0500

    puppetboard/setup.py: Version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
     sys.exit()
 
-VERSION = "0.1.0"
+VERSION = "0.1.1"
 
 with codecs.open('README.rst', encoding='utf-8') as f:
     README = f.read()
