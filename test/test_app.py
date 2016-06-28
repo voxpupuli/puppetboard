a42ceb36e78fc8ab4f1d46ae326effc5944ae040
Mike Terzo <Michael.Terzo@emc.com>
2016-06-28 18:28:21 -0400

    Initial travis-ci integration

---
+++ test/test_app.py
@@ -0,0 +1,19 @@
+import os
+from puppetboard import app
+import unittest
+import tempfile
+
+
+class AppTestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_first_test(self):
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
