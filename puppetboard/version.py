4cb5980204db58eca5b853c46d87626d7ee13186
redref <redref@users.noreply.github.com>
2017-02-13 06:25:19 +0100

    Requirements suggestions + version fix. (#2)
    * Inherit from requirements.txt instead of duplicating
    * Requirements in setup.py + tox.ini

--- puppetboard/version.py
+++ puppetboard/version.py
@@ -2,4 +2,4 @@
 # Puppetboard version module
 #
 
-__version__ = '0.3.0-dev'
+__version__ = '0.3.0.dev0'
--- setup.py
+++ setup.py
@@ -1,21 +1,103 @@
 import sys
 import os
 import codecs
+import re
 
 from setuptools import setup, find_packages
+from puppetboard.version import __version__
+
+install_requires = [
+    "Flask >= 0.10.1",
+    "Flask-WTF >= 0.12, <= 0.13",
+    "WTForms >= 2.0, < 3.0",
+    "pypuppetdb >= 0.3.2, < 0.4.0",
+]
+
+install_pinned_requires = [
+    "Jinja2 >= 2.7.2",
+    "MarkupSafe >= 0.19",
+    "Werkzeug >= 0.11.10",
+    "itsdangerous >= 0.23",
+    "requests == 2.6.0",
+]
+
+tests_require = [
+    "pytest >= 3.0.1",
+    "pytest-pep8 >= 1.0.5",
+    "pytest-cov >= 2.2.1",
+    "pytest-mock >= 1.5.0",
+    "mock >= 1.3.0",
+    "bandit",
+    "beautifulsoup4 >= 4.5.3",
+]
+
+tests_pinned_requires = [
+    "pep8 >= 1.6.2",
+    "coverage >= 4.0",
+    "cov-core >= 1.15.0",
+]
+if sys.version_info < (2, 7):
+    tests_pinned_requires.append("unittest2 >= 1.1.0")
+
+docker_requires = [
+    "gunicorn == 19.6.0",
+]
+
+version_parse = r'^(?P<name>[^\s<=>]+)\s*(?P<pick>[^;]*)(?P<test>(|;.*))$'
 
 
-if sys.argv[-1] == 'publish':
-    os.system('python setup.py sdist upload')
-    sys.exit()
+def unpin(requirements):
+    for i, package in enumerate(requirements):
+        requirements[i] = re.sub(version_parse, r'\g<name> \g<test>', package)
 
-exec(open('puppetboard/version.py').read())
+
+def pin(requirements):
+    for i, package in enumerate(requirements):
+        match = re.match(version_parse, package)
+        version = re.sub(
+            r'^.*((>=|==)\s*(?P<lower>[^,]+)).*$', r'== \g<lower>',
+            match.group('pick'))
+        requirements[i] = "%s %s%s" % (
+            match.group('name'), version, match.group('test'))
 
 with codecs.open('README.rst', encoding='utf-8') as f:
     README = f.read()
 
 with codecs.open('CHANGELOG.rst', encoding='utf-8') as f:
     CHANGELOG = f.read()
+
+test_deps = False
+if os.environ.get('TEST_DEPS') in ('y', 'true', 'True', 't'):
+    test_deps = True
+deps_resolve = os.environ.get('DEPS_RESOLVE')
+
+for arg in sys.argv[:]:
+    if arg == 'publish':
+        sys.argv = [sys.argv[0], 'sdist', 'upload']
+        break
+    elif arg == 'docker':
+        sys.argv.remove(arg)
+        install_requires.extend(docker_requires)
+    elif arg == 'unpinned':
+        sys.argv.remove(arg)
+        deps_resolve = 'UNPINNED'
+    elif arg == 'pinned':
+        sys.argv.remove(arg)
+        deps_resolve = 'PINNED'
+    elif arg == 'with_test':
+        sys.argv.remove(arg)
+        test_deps = True
+
+if test_deps:
+    install_requires.extend(tests_require)
+
+if deps_resolve == 'UNPINNED':
+    unpin(install_requires)
+elif deps_resolve == 'PINNED':
+    install_requires.extend(install_pinned_requires)
+    if test_deps:
+        install_requires.extend(tests_pinned_requires)
+    pin(install_requires)
 
 setup(
     name='puppetboard',
@@ -29,12 +111,8 @@ setup(
     include_package_data=True,
     long_description='\n'.join((README, CHANGELOG)),
     zip_safe=False,
-    install_requires=[
-        "Flask >= 0.10.1",
-        "Flask-WTF >= 0.12, <= 0.13",
-        "WTForms >= 2.0, < 3.0",
-        "pypuppetdb >= 0.3.0, < 0.4.0",
-    ],
+    install_requires=install_requires,
+    test_requires=tests_require,
     keywords="puppet puppetdb puppetboard",
     classifiers=[
         'Development Status :: 3 - Alpha',
