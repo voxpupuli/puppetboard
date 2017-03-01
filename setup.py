e3fb10abe868f5797f304fa5ed0f8bc3d8f3d6a5
Mike Terzo <mike@terzo.org>
2017-03-01 12:13:07 -0500

    extras

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ params = dict(
     zip_safe=False,
     setup_requires=['pytest-runner >=2.0,<3dev'],
     install_requires=requirements,
-    extra_require= {'test': requirements_test},
+    extras_require={'test': requirements_test},
     keywords="puppet puppetdb puppetboard",
     classifiers=[
         'Development Status :: 3 - Alpha',
