6fea59257b51cd184995ac477880b38518093bf6
John Bond <github@johnbond.org>
2020-03-23 13:46:48 +0100

    python3.5 (#551): add python 3.5 back to the test matrix
    This PR reverts #531 to support for python3.5 back to the test matrix.
    
    closes #551

--- setup.py
+++ setup.py
@@ -67,6 +67,7 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: POSIX',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
