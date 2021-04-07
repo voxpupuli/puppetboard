7fbc7da0226c98b0603290ded2ee61d194023d07
Romain Tartière <romain@blogreen.org>
2021-04-06 18:36:21 -1000

    Improve facts columns balancing
    Grouping facts by first letter is inefficient when a lot of facts begin
    with some specific letters.  E.g. with multiple network interfaces.

--- test/test_app.py
+++ test/test_app.py
@@ -769,7 +769,7 @@ def test_facts_view_empty_when_no_facts(client,
 
     searchable = soup.find('div', {'class': 'searchable'})
     vals = searchable.find_all('div', {'class': 'column'})
-    assert len(vals) == 1
+    assert len(vals) == 0
 
 
 def test_fact_view_with_graph(client, mocker,
