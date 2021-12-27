fbe0a1ec68f76c82b2a98eea316897d04a297545
German Gutierrez <german.gutierrez@gmail.com>
2021-12-27 11:21:47 +0100

    using url_for() in prettyprint

--- puppetboard/utils.py
+++ puppetboard/utils.py
@@ -95,7 +95,7 @@ def prettyprint(value):
         html += "<tr>"
         for k in e:
             if k == "certname":
-                html += "<td> <a href = '/node/" + formatvalue(e[k]) + "' >" + formatvalue(e[k]) + "</a></td>"
+                html += "<td> <a href = '" + url_for('node', node_name=formatvalue(e[k])) + "' >" + formatvalue(e[k]) + "</a></td>"
             else:
                 html += "<td>" + formatvalue(e[k]) + "</td>"
         html += "</tr>"
