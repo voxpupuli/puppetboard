2af188fb2d5167d62b92f2e2161042b3697256a1
Greg Dubicki <grzegorz.dubicki@gmail.com>
2022-02-19 22:10:33 +0000

    Refactor: pull out inventory

---
+++ puppetboard/views/inventory.py
@@ -0,0 +1,111 @@
+import json
+import logging
+from datetime import datetime, timedelta
+from itertools import tee
+from json import dumps
+from urllib.parse import unquote, quote_plus
+
+import commonmark
+from flask import (
+    render_template, abort, url_for,
+    Response, stream_with_context, request, session, jsonify
+)
+from pypuppetdb.QueryBuilder import (ExtractOperator, AndOperator,
+                                     EqualsOperator, FunctionOperator,
+                                     NullOperator, OrOperator,
+                                     LessEqualOperator, RegexOperator,
+                                     GreaterEqualOperator)
+from requests.exceptions import HTTPError
+
+from puppetboard.core import get_app, get_puppetdb, environments
+from puppetboard.dailychart import get_daily_reports_chart
+from puppetboard.forms import ENABLED_QUERY_ENDPOINTS, QueryForm
+from puppetboard.utils import (get_or_abort, get_or_abort_except_client_errors, yield_or_stop,
+                               get_db_version, parse_python, check_env)
+from puppetboard.version import __version__
+
+
+app = get_app()
+puppetdb = get_puppetdb()
+
+
+def inventory_facts():
+    # a list of facts descriptions to go in table header
+    headers = []
+    # a list of inventory fact names
+    fact_names = []
+
+    # load the list of items/facts we want in our inventory
+    try:
+        inv_facts = app.config['INVENTORY_FACTS']
+    except KeyError:
+        inv_facts = [('Hostname', 'fqdn'),
+                     ('IP Address', 'ipaddress'),
+                     ('OS', 'lsbdistdescription'),
+                     ('Architecture', 'hardwaremodel'),
+                     ('Kernel Version', 'kernelrelease')]
+
+    # generate a list of descriptions and a list of fact names
+    # from the list of tuples inv_facts.
+    for desc, name in inv_facts:
+        headers.append(desc)
+        fact_names.append(name)
+
+    return headers, fact_names
+
+
+@app.route('/inventory', defaults={'env': app.config['DEFAULT_ENVIRONMENT']})
+@app.route('/<env>/inventory')
+def inventory(env):
+    """Fetch all (active) nodes from PuppetDB and stream a table displaying
+    those nodes along with a set of facts about them.
+
+    :param env: Search for facts in this environment
+    :type env: :obj:`string`
+    """
+    envs = environments()
+    check_env(env, envs)
+    headers, fact_names = inventory_facts()
+
+    return render_template(
+        'inventory.html',
+        envs=envs,
+        current_env=env,
+        fact_headers=headers)
+
+
+@app.route('/inventory/json', defaults={'env': app.config['DEFAULT_ENVIRONMENT']})
+@app.route('/<env>/inventory/json')
+def inventory_ajax(env):
+    """Backend endpoint for inventory table"""
+    draw = int(request.args.get('draw', 0))
+
+    envs = environments()
+    check_env(env, envs)
+    headers, fact_names = inventory_facts()
+
+    query = AndOperator()
+    fact_query = OrOperator()
+    fact_query.add([EqualsOperator("name", name) for name in fact_names])
+    query.add(fact_query)
+
+    if env != '*':
+        query.add(EqualsOperator("environment", env))
+
+    facts = puppetdb.facts(query=query)
+
+    fact_data = {}
+    for fact in facts:
+        if fact.node not in fact_data:
+            fact_data[fact.node] = {}
+        fact_data[fact.node][fact.name] = fact.value
+
+    total = len(fact_data)
+
+    return render_template(
+        'inventory.json.tpl',
+        draw=draw,
+        total=total,
+        total_filtered=total,
+        fact_data=fact_data,
+        columns=fact_names)
