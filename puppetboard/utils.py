9dfaad9af6be6ce79951b6d67267d1043493deb1
Greg Dubicki <grzegorz.dubicki@gmail.com>
2022-06-27 12:01:33 +0200

    Don't reparse the version + more logical order (thx ekohl)

--- puppetboard/utils.py
+++ puppetboard/utils.py
@@ -38,23 +38,24 @@ def check_db_version(puppetdb):
     """
     try:
         version = puppetdb.current_version()
-
         log.info(f"PuppetDB version: {version}")
+
+        parsed_version = parse(version)
+
+        if parsed_version < parse('5.2.13'):
+            log.error("The minimum supported version of PuppetDB is 5.2.13 (with v2 metrics API)")
+            sys.exit(1)
 
         # Puppet Server is enforcing new metrics API (v2)
         # starting with versions 6.9.1, 5.3.12, and 5.2.13
-        if parse('5.2.0') <= parse(version) < parse('5.2.13'):
+        if parse('5.2.0') <= parsed_version < parse('5.2.13'):
             log.error("For PuppetDB 5.2.x version >= 5.2.13 is required (with v2 metrics API)")
             sys.exit(1)
-        if parse('5.3.0') <= parse(version) < parse('5.3.13'):
+        if parse('5.3.0') <= parsed_version < parse('5.3.13'):
             log.error("For PuppetDB 5.3.x version >= 5.3.13 is required (with v2 metrics API)")
             sys.exit(1)
-        if parse('6.0.0') <= parse(version) < parse('6.9.1'):
+        if parse('6.0.0') <= parsed_version < parse('6.9.1'):
             log.error("For PuppetDB 6.x version >= 6.9.1 is required (with v2 metrics API)")
-            sys.exit(1)
-
-        if parse(version) < parse('5.2.13'):
-            log.error("The minimum supported version of PuppetDB is 5.2.13 (with v2 metrics API)")
             sys.exit(1)
 
     except HTTPError as e:
