d09f0163f1e5a0f1bf05a0fea075bd83f823ebd0
Melchior NOGUES <m3lchior@gmail.com>
2022-07-24 13:10:44 +0200

    Fix replacement c3 by billboard

--- puppetboard/static/js/dailychart.js
+++ puppetboard/static/js/dailychart.js
@@ -1,40 +1,54 @@
 jQuery(function ($) {
-  function generateChart(el) {
-    var url = "daily_reports_chart.json";
-    var certname = $(el).attr('data-certname');
-    if (typeof certname !== typeof undefined && certname !== false) {
-      // truncate /node/certname from URL, to determine path to json
-      url = window.location.href.replace(/\/node\/[^/]+$/,'') + "/daily_reports_chart.json?certname=" + certname;
-    }
-    d3.json(url, function(data) {
-      var chart = c3.generate({
-        bindto: '#dailyReportsChart',
-        data: {
-          type: 'bar',
-          json: data['result'],
-          keys: {
-            x: 'day',
-            value: ['failed', 'changed', 'unchanged'],
-          },
-          groups: [
-            ['failed', 'changed', 'unchanged']
-          ],
-          colors: {  // Must match CSS colors
-            'failed':'#AA4643',
-            'changed':'#4572A7',
-            'unchanged':'#89A54E',
-          }
-        },
-        size: {
-          height: 160
-        },
-        axis: {
-          x: {
-            type: 'category'
-          }
-        }
-      });
-    });
+  let url = "daily_reports_chart.json";
+  let certname = $("#dailyReportsChart").data("certname");
+  let days = parseInt($("#dailyReportsChart").data("days"));
+  let defaultJSON = []
+
+  for (let index = days-1; index >= 0; index--) {
+    defaultJSON.push({
+      day: moment().startOf('day').subtract(index, 'days').format('YYYY-MM-DD'),
+      unchanged: 0,
+      changed: 0,
+      failed: 0
+    })
   }
-  generateChart($("#dailyReportsChart"));
-});
+
+  let chart = bb.generate({
+    bindto: "#dailyReportsChart",
+    data: {
+      type: "bar",
+      json: defaultJSON,
+      keys: {
+        x: "day",
+        value: ["failed", "changed", "unchanged"],
+      },
+      groups: [["failed", "changed", "unchanged"]],
+      colors: {
+        // Must match CSS colors
+        failed: "#AA4643",
+        changed: "#4572A7",
+        unchanged: "#89A54E",
+      },
+    },
+    size: {
+      height: 160,
+    },
+    axis: {
+      x: {
+        type: "category",
+      },
+    },
+  });
+
+  if (typeof certname !== typeof undefined && certname !== false) {
+    // truncate /node/certname from URL, to determine path to json
+    url =
+      window.location.href.replace(/\/node\/[^/]+$/, "") +
+      "/daily_reports_chart.json?certname=" +
+      certname
+  }
+
+  $.getJSON(url, function(data) {
+    chart.load({json: data.result})
+  });
+})
