5eefe110d828773c818d9d488901c8448d39cdf1
Melchior NOGUES <m3lchior@gmail.com>
2022-09-26 12:40:15 +0200

    Reimplement filtering fact name feature

--- puppetboard/static/js/utils.js
+++ puppetboard/static/js/utils.js
@@ -1,4 +1,18 @@
 /**
+ * Simple debounce function
+ */
+debounce = function(cb, delay = 250) {
+  let timeout
+
+  return (...args) => {
+    clearTimeout(timeout)
+    timeout = setTimeout(() => {
+      cb(...args)
+    }, delay)
+  }
+}
+
+/**
  * Transform date string with tz to shorter string.
  * If localisation enabled in config,  date is timezoned with browser
  * If localisation disabled in config, date is UTC
@@ -20,6 +34,52 @@ $.fn.transformDatetime = function (format = "MMM DD YYYY - HH:mm:ss") {
   return this
 }
 
+
+/**
+ * Enable filtering on fact list
+ *
+ * If text is entered as input, it will use that text to create a regexp
+ * that will match each fact name.
+ * If a match is found, make sure the fact name is visible.
+ * If no match is found, make sure the fact name is hidden.
+ * Then, we display all segments and hide all those that do not have
+ * at least a visible fact name.
+ */
+$.fn.factList = function () {
+  this.each(function() {
+    const $el = $(this)
+    const $input = $el.find("input[name=filter]")
+    const $segments = $el.find(".segment")
+    const $facts = $el.find(".segment li")
+
+    $input.on("input", debounce(function (e) {
+      const text = $input.val()
+
+      if (!text) {
+        $segments.show()
+        $facts.show()
+        return
+      }
+
+      const pattern = new RegExp(text, "i")
+
+      $facts.each(function () {
+        const $fact = $(this)
+
+        if (pattern.test($fact.text())) {
+          $fact.show()
+        } else {
+          $fact.hide()
+        }
+      })
+      $segments.show()
+      $segments.not(':has(li:visible)').hide()
+    }, 250))
+  })
+
+  return this
+}
+
 $(document).ready(function () {
   $("[data-localise]").transformDatetime()
 
@@ -27,4 +87,6 @@ $(document).ready(function () {
   $("#main-table-search").on("input", function () {
     dataTable.search(this.value).draw()
   })
+
+  $('#fact-list').factList()
 })
